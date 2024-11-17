c631160cc4ff69786cf71dc730db189a070a89e3
laisholdwin10 <laishixuan123@gmail.com>
2024-11-17 23:54:04 +0800

    update 16to8 function, to use average windows wide to avoid the brightness change of images

--- workflow_tools/16to8bit.py
+++ workflow_tools/16to8bit.py
@@ -8,6 +8,10 @@ from tqdm import tqdm
 import os
 import numpy as np
 
+def average(values: list):
+    return sum(values) / len(values)
+
+
 def convert_to_8bit(path_in, path_out):
 
     # Create the output directory if it does not exist
@@ -19,19 +23,22 @@ def convert_to_8bit(path_in, path_out):
     names = [os.path.basename(item) for item in image_names]
     image_files = fb.read_images(image_names, gray='gray', read_all=True)
 
-    # min = 255
-    # max = 0
-    # for image_file in image_files:
-    #     if image_file.min() < min:
-    #         min = image_file.min()
-    #     if image_file.max() > max:
-    #         max = image_file.max()
-
-    for i, image_file in tqdm(enumerate(image_files)):
+    min_values = []
+    max_values = []
+    for image_file in image_files:
+        min_values.append(image_file.min())
+        max_values.append(image_file.max())
+    my_min = average(min_values)
+    my_max = average(max_values)
+    
+    for i, image_file in enumerate(tqdm(image_files)):
         
-        min = image_file.min()
-        max = image_file.max()
-        image_8bit = ((image_file - min) / (max - min) * 255).astype(np.uint8)
+        # For image's value above my_max, set it to my_max
+        image_file[image_file > my_max] = my_max
+
+        # For image's value below my_min, set it to my_min
+        image_file[image_file < my_min] = my_min
+        image_8bit = ((image_file - my_min) / (my_max - my_min) * 255).astype(np.uint8)
 
         # # Convert the image to 8-bit
         # image_8bit = (image_file / 256).astype('uint8')
@@ -45,7 +52,7 @@ def convert_to_8bit(path_in, path_out):
 
     print("\033[1;3mConversion to 8-bit completed!\033[0m")
 
-if __name__ == '__main__':
-    path_in = 'f:/3.Experimental_Data/Soils/Quzhou_Henan/Soil.column.0028/16bits/1.Reconstruct/'
-    path_out = 'f:/3.Experimental_Data/Soils/Quzhou_Henan/Soil.column.0028/8bits/1.Reconstruct/'
-    convert_to_8bit(path_in, path_out)
+# Example usage
+path_in = 'f:/3.Experimental_Data/Soils/Quzhou_Henan/Soil.column.0028/16bits/1.Reconstruct/'
+path_out = 'f:/3.Experimental_Data/Soils/Quzhou_Henan/Soil.column.0028/8bits/1.Reconstruct/'
+convert_to_8bit(path_in, path_out)
