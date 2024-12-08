fix bug, wrong order of width and depth

src/API_functions/Images/read_raw.py
@@ -1,20 +1,20 @@
 import cv2
 import numpy as np
 
-def read_raw(file_path: str, width: int, height: int, depth: int, dtype=np.dtype('<i2'), virtual=True) -> np.ndarray:
+def read_raw(file_path: str, height: int, width: int, depth: int, dtype=np.dtype('<i2'), virtual=True) -> np.ndarray:
     """
     Read a raw file into a numpy memmap array or a numpy array.
     """
 
     if virtual:
-        arr = np.memmap(file_path, dtype=dtype, mode='r', shape=(depth, width, height))
+        arr = np.memmap(file_path, dtype=dtype, mode='r', shape=(depth, height, width))
         # do not read the whole file into memory
         return arr
 
     else:
         with open(file_path, 'rb') as f:
             arr = np.fromfile(f, dtype=dtype)
-            arr = arr.reshape((depth, width, height))
+            arr = arr.reshape((depth, height, width))
             return arr
 
 if __name__ == '__main__':