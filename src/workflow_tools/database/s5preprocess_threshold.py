2cecd96ab9de50ed7fa6ec9438754be7594457eb
laisholdwin10 <laishixuan123@gmail.com>
2024-12-17 22:48:40 +0800

    reconstruct this file

--- src/workflow_tools/database/s5preprocess_threshold.py
+++ src/workflow_tools/database/s5preprocess_threshold.py
@@ -1,8 +1,5 @@
-#%%
-
 import numpy as np
 import cv2 as cv
-import os
 import sys
 
 sys.path.insert(0, "c:/Users/laish/1_Codes/Image_processing_toolchain/")
@@ -13,74 +10,58 @@ from src.API_functions.Images import file_batch as fb
 from src.API_functions.Soils import pre_process
 from src.API_functions.Soils import threshold_position_independent as tmi
 
-#%%
-# Adjust the windows of the images
 
-for i in range(10, 22):
-    path_in = f'f:/3.Experimental_Data/Soils/Dongying_Tiantan-Hospital/Soil.column.{i:04d}/3.Precheck/images/'
-    path_out = f'f:/3.Experimental_Data/Soils/Dongying_Tiantan-Hospital/Soil.column.{i:04d}/4.Preprocess/Remap/'
-
+def batch_process_images(path_in, path_out, process_function, file_pattern: str=None, extension: str='tif', pattern_include: bool=False):
+    """
+    Batch process images with a sequence of processing steps
+    """
+    path_in = Path(path_in)
     path_out = Path(path_out)
-    if not path_out.exists():
-        path_out.mkdir(parents=True)
-
-    fb.windows_adjustment(path_in, path_out, min=None, max=None)
-
-
-#%%
-# Thresholding the images, to produce the label, if needed
-# !!! do not run this cell if the images are already thresholded
-
-for i in [28, 29, 30, 31, 32, 33, 34]:
-    path_in = f'f:/3.Experimental_Data/Soils/Quzhou_Henan/Soil.column.{i:04d}/4.Preprocess/Remap/'
-    image_lists = fb.get_image_names(path_in, None, 'tif')
-    image_names = [os.path.basename(item) for item in image_lists]
-    images = fb.read_images(image_lists, gray='gray', read_all=True)
-    images = np.array(images)
-
-    for j, image in enumerate(tqdm(images)):
-        mask = (image > 0) & (image < 1)
-        
-        # Pass both the original image and mask to kmeans_3d
-        image = tmi.kmeans_3d(image, mask)
-        # image = 1 - image
-        
-        path_out=f'f:/3.Experimental_Data/Soils/Quzhou_Henan/Soil.column.{i:04d}/4.Threshold/Remap-kmeans/'
-        if not os.path.exists(path_out):
-            os.makedirs(path_out)
-        cv.imwrite(os.path.join(path_out, image_names[j]), image)
-    break
-
-#%%
-# Batch inverse the images
-def invert_image(image):
-    return 1.0 - image
-
-
-def batch_images(path_in, path_out):
-    image_lists = fb.get_image_names(path_in, None, 'tif')
-
-    if not os.path.exists(path_out):
-        os.makedirs(path_out)
+    image_lists = fb.get_image_names(str(path_in), None, extension)
     
-    for image_path in tqdm(image_lists, desc="Inverting images"):
-        # Read image
-        image = cv.imread(image_path, cv.IMREAD_UNCHANGED)
-        
-        # Invert image
-        image = pre_process.median(image, kernel_size=5)
-        image = pre_process.clahe_float32(image)
-        mean = np.mean(image)
-        image = image - mean
+    if file_pattern:
+        if pattern_include:
+            image_lists = [item for item in image_lists if Path(item).name.startswith(file_pattern)]
+        else:
+            image_lists = [item for item in image_lists if not Path(item).name.startswith(file_pattern)]
 
-        name = os.path.basename(image_path)
-        image_path = os.path.join(path_out, name)
-        
-        # Save with same name in same location
-        cv.imwrite(image_path, image)
+    path_out.mkdir(parents=True, exist_ok=True)
 
-# Example usage
-path_in = r'g:\DL_Data_raw\version4-classes\3.Harmonized\image'
-path_out = r'g:\DL_Data_raw\version4-classes\5.Preprocess'
-batch_images(path_in, path_out)
+    for image_path in tqdm(image_lists):
+        image = cv.imread(str(image_path), cv.IMREAD_UNCHANGED)
+        processed_image = process_function(image)
 
+        output_path = path_out / Path(image_path).name.replace('harmonized', 'preprocessed')
+        cv.imwrite(str(output_path), processed_image)
+
+
+if __name__ == "__main__":
+
+    def process_pipeline(image):
+        # image = pre_process.median(image, 5)
+        # image = pre_process.clahe(image)
+        # image = 1 - np.mean(image)
+
+        # image = pre_process.median(image, 5)
+        # image2 = image.copy()
+        # _, threshold = tmi.kmeans_3d(image2, return_threshold=True)
+        # image = tmi.user_threshold(image, threshold-0.15)
+
+        image = 1 - image
+        return image
+
+    path_in = Path(r'g:\DL_Data_raw\version6-large\4.Converted\label')
+    path_out = Path(r'g:\DL_Data_raw\version6-large\4.Converted\label')
+    extension = 'tif'
+    
+    batch_process_images(
+        path_in=path_in,
+        path_out=path_out,
+        process_function=process_pipeline,
+
+        # Select only '0035' or exclude '0035'
+        file_pattern=None,
+        pattern_include=True,
+
+        extension=extension
+    )
