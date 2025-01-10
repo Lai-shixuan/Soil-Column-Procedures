9b116dabb73d394981fc3d52cefcfbf50a829052
shixuan-wsl <laishixuan123@gmail.com>
2025-01-11 01:27:18 +0800

    the same

--- src/workflow_tools/dl_config.py
+++ src/workflow_tools/dl_config.py
@@ -27,7 +27,7 @@ from src.workflow_tools.model_online import mcc
 def get_parameters() -> Dict[str, Any]:
     config_dict = {
         # Title and seed
-        'wandb': '34.3-alpha3080-cos150-ramp50-morecons-conf',
+        'wandb': '34.3-alpha3080-cos150-ramp50-morecons',
         'seed': 3407,
         
         # PC related parameters
@@ -62,7 +62,7 @@ def get_parameters() -> Dict[str, Any]:
         # Add semi-supervised parameters
         'mode': 'semi',             # 'supervised' or 'semi'
         'unlabel_batch_size': 7,
-        'consistency_weight': 0.75,
+        'consistency_weight': 0.66,
         'consistency_rampup': 50,
         'teacher_alpha': 0.999,
 
