b7a5d9be9a28dd9aaaa0ba6e4c9487a33abaf4c6
shixuan-wsl <laishixuan123@gmail.com>
2025-12-08 22:38:38 +0800

    19-semi-conf-longlongref

--- src/workflow_tools/dl_config.py
+++ src/workflow_tools/dl_config.py
@@ -15,7 +15,7 @@ from src.workflow_tools.model_online import mcc
 def get_parameters() -> Dict[str, Any]:
     config_dict = {
         # Title and seed
-        'wandb': '18-semi-conf-longref',
+        'wandb': '19-semi-conf-longlongref',
         # 'wandb': '34.9-alpha3080-cos150-ramp50-larger-batch-no-conf',
         'seed': 3407,
         
@@ -52,9 +52,9 @@ def get_parameters() -> Dict[str, Any]:
         'mode': 'semi',             # 'supervised' or 'semi'
         'unlabel_batch_size': 6,
         'consistency_weight': 0.5,
-        'consistency_rampup': 13,
-        'teacher_alpha_initial_epoch': 7,  # Copy model directly before this epoch
-        'teacher_alpha_mid_epoch': 30,      # Use mid alpha before this epoch
+        'consistency_rampup': 26,
+        'teacher_alpha_initial_epoch': 14,  # Copy model directly before this epoch
+        'teacher_alpha_mid_epoch': 45,      # Use mid alpha before this epoch
         'teacher_alpha_mid': 0.99,          # Alpha value from initial to mid epoch
         'teacher_alpha': 0.999,             # Alpha value after mid epoch
 
