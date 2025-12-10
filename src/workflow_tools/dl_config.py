f94776dde5b9f406c5b087a495c06bd29fcdf9d1
shixuan-wsl <laishixuan123@gmail.com>
2025-12-10 22:45:49 +0800

    30-rubbish-out

--- src/workflow_tools/dl_config.py
+++ src/workflow_tools/dl_config.py
@@ -15,7 +15,7 @@ from src.workflow_tools.model_online import mcc
 def get_parameters() -> Dict[str, Any]:
     config_dict = {
         # Title and seed
-        'wandb': '29-cut_mix-all-image',
+        'wandb': '30-rubbish-out',
         # 'wandb': '34.9-alpha3080-cos150-ramp50-larger-batch-no-conf',
         'seed': 3407,
         
